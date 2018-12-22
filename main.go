ab11420b4a9d49bffe3574392c3286c9a1f1d00a
Paul Crane <paul@acheronsoft.com>
2018-12-21 22:49:04 -0600

    Add initial support for command line arguments.

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"github.com/levigross/grequests"
+	"github.com/urfave/cli"
+)
+
+func getMacAddressName(address string) {
+	resp, err := grequests.Get("https://api.macvendors.com/"+address, nil)
+
+	if err != nil {
+	}
+
+	fmt.Println(resp.String())
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "getmacname"
+	app.Usage = "HELP"
+
+	err := app.Run(os.Args)
+
+	if err != nil {
+		getMacAddressName("FC:FB:FB:01:FA:21")
+	}
 }
