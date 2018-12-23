d47db14a73b22c4115a151e0826ca0f49bf907fb
Paul Crane <paul@acheronsoft.com>
2018-12-23 01:56:43 -0600

    Add better checking of MAC Address format using regex.

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
+	"regexp"
 
 	"github.com/levigross/grequests"
 	"github.com/urfave/cli"
@@ -19,6 +19,11 @@ func getMacAddressName(address string) {
 	fmt.Println(resp.String())
 }
 
+func isValidAddressFormat(address string) bool {
+	match, _ := regexp.MatchString("^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$", address)
+	return match
+}
+
 func main() {
 	app := cli.NewApp()
 	app.Name = "getmacname"
@@ -27,9 +32,9 @@ func main() {
 	app.Action = func(c *cli.Context) error {
 		if c.NArg() > 0 {
 			var address = c.Args().Get(0)
+			var isValidAddress = isValidAddressFormat(address)
 
-			// INFO Could probably use a regex here to determine the validity of the passed MAC Address.
-			if strings.Count(address, ":") == 5 {
+			if isValidAddress == true {
 				getMacAddressName(address)
 			} else {
 				fmt.Println("Invalid MAC Address!")
